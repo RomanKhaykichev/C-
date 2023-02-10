66ac4d84fa1903260c0d7e0c0c2a2ee64938c6a0
Roman_Khaykichev <mr.romanx@mail.ru>
2023-02-11 00:56:56 +0300

    Delete folder Kvadrat- not useful

--- Practise/Kvadrat/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿//Найти квадратный корень числа
-
-Console.Clear();
-Console.WriteLine("Введите число: ");
-
-int value = Convert.ToInt32(Console.ReadLine()); // Конвертирует вводимое число в числовую
-
-Console.WriteLine("Квадратный корень числа: " + value*value);
-Console.WriteLine("Другой вариант: "+ Math.Pow(value,2));// Мат формула
