ce654decba31e14b24c5cb2571bd1307843da14d
Roman_Khaykichev <mr.romanx@mail.ru>
2023-02-10 21:13:48 +0300

    квадрат умнож

---
+++ Practise/Kvadrat/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("квадрат числа " + value +" равен: " + value* value);
+
+Console.WriteLine("Способ 2. Квадрат числа " + value + " равен: " + Math.Pow(value,2)); // 2 вариант
+
