a4b23be97310fcd2abd44bc45e27f8c7577f61ee
Roman_Khaykichev <mr.romanx@mail.ru>
2023-02-12 20:26:38 +0300

    Добавлены примеры 008-011 со второй лекции

---
+++ Examples/Example008_IntroMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿//Найти смаксимальное из 9 чисел
+Console.Clear();
+
+int Max(int arg1, int arg2, int arg3) //Функция Мах сравнения по 3
+{
+    int result = arg1;
+    if (arg2>result) result = arg2;
+    if (arg3>result) result = arg3;
+    return result;
+}
+int a1 =2;
+int b1 =6;
+int c1 =45;
+int a2 =25;
+int b2 =14;
+int c2 =456;
+int a3 =12;
+int b3 =14;
+int c3 =78;
+
+// int max1 = Max(a1,b1,c1);
+// int max2 = Max(a2,b2,c2);
+// int max3 = Max(a3,b3,c3);
+// int max = Max(max1,max1,max3);
+
+int max=Max(
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
+
+Console.Write(max);
---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Void - Метод(функция) который ничего не возвращает
+1) Определили массив (строка 45)
+2) FillArray - метод заполнили массив
+3) PrintArray - метод Распечатали массив
+*/
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index<lenght)
+    {
+        collection[index]=new Random().Next(1,10); // новое случайное целое число из диапазона 1-10
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //вместо 0 поставили -1 (в случае если элемента нет)
+
+    while (index<count)
+    {
+        if (collection[index]==find)
+        {
+            position=index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10]; // new - Создай новый массив, в котором ьудет 10 элементов
+
+FillArray(array);
+
+array[4]=4;//добавили доп 4 для проверки
+array[6]=4;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
