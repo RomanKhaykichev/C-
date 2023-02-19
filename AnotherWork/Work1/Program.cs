f458296aaf8433e4768bf64cd0c6eacf1e851874
Roman_Khaykichev <mr.romanx@mail.ru>
2023-02-19 16:04:00 +0300

    Добавлен файл для других работ

---
+++ AnotherWork/Work1/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+#region Задача 19.1
+Console.WriteLine("---Задача19.1: Проверка на палиндром --- ");
+int Print(string number)
+{
+    Console.Write(number);
+    int numberST = int.Parse(Console.ReadLine());
+    return numberST;
+}
+string Palindrom(int num)
+{
+    string number = num.ToString();
+    bool flag = false;
+    for (int i = 0; i < number.Length / 2; i++)
+    {
+        if (number[i] == number[number.Length - 1 - i])
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+        }
+    }
+    return (flag ? "Yes" : "No");
+}
+
+int number = Print("hh: ");
+Palindrom(number);
+
+#endregion*/
