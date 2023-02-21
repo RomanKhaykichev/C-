80d70c59a870ade97ae44cfad41d94ac2004e1c3
Roman_Khaykichev <mr.romanx@mail.ru>
2023-02-21 23:19:28 +0300

    HomeWork 4 ( 1/2)

---
+++ Homework/Lesson4/Program.cs
@@ -0,0 +1,64 @@
+﻿//Урок 4. Функции
+Console.Clear();
+/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A
+в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+/*#region Task25
+Console.WriteLine("---Задача 25: число А в в натуральную степень В---");
+
+int Print(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int Pow(int a, int b)
+{
+    int degreeNum = a;
+    for (int i = 1; i < b; i++)
+    {
+        degreeNum *= a;
+    }
+    return degreeNum;
+
+}
+
+int a = Print("Введите число А: ");
+int b = Print("Введите степень В: ");
+Console.WriteLine($"Число А->{a} в степени В->{b} = {Pow(a,b)}");
+Console.WriteLine();
+#endregion*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+#region Task27
+int Print(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] SumNumbersInNum(int[] number)
+{
+    int sum = 0;
+    for (int i = 0; i < number.Lenght; i++)
+    {
+        sum += number[i];
+    }
+    return sum;
+}
+
+int numLenght = Print("Введите число: ");
+int[] array = new int[num.Lenght];
+int sum = SumNumbersInNum(array);
+Console.WriteLine($"Сумма цифр в числе = {sum}");
+
+
+Console.WriteLine();
+#endregion
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
