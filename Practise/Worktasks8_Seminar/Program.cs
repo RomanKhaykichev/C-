7ad3230300c5c884531d465cefdf95cb2862cd68
Roman_Khaykichev <mr.romanx@mail.ru>
2023-03-09 20:59:37 +0300

    Добавлен семинар 8

---
+++ Practise/Worktasks8_Seminar/Program.cs
@@ -0,0 +1,335 @@
+﻿Console.Clear();
+
+/*Задача 60: Найти разницу между максимальным и минимальным значением элемента
+в двумерном массиве заполненном случайными числами
+
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1  
+Разница между максимальным и минимальным элементом в массиве = 79*/
+
+/*
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+Console.WriteLine($"Разница между максимальным и минимальным элементом в массиве = {GetMaxValue(array) - GetMinValue(array) }");
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetMinValue(int[,] inputArray){
+    
+    int min = inputArray[0,0];
+
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if(inputArray[i,j] < min)
+                min = inputArray[i,j];
+        }
+    }
+    return min;
+}
+
+int GetMaxValue(int[,] inputArray){
+    
+    int max = inputArray[0,0];
+
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            if(inputArray[i,j] > max)
+                max = inputArray[i,j];
+        }
+    }
+    return max;
+}
+*/
+
+
+/*Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+В случае, если это невозможно, программа должна вывести сообщение для пользователя.*/
+/*
+int Print(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+int[,] FillArray(int rows, int columns, int min, int max)
+{
+    int[,] array = new int[rows, columns];
+    Random num = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = num.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
+/*int[,] SwapRowsColumns(int[,] array)
+{
+    int[,] swapArray = new int[array.GetLength(1), array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            swapArray[j, i] = array[i, j];
+        }
+    }
+    return swapArray;
+}
+bool CheckLenghtArray(int[,] array, int[,] swapArray)
+{
+
+    if (array.GetLength(0) == swapArray.GetLength(0) && array.GetLength(1) == swapArray.GetLength(1)) return true;
+    else Console.WriteLine("Это невозможно! не корректные данные"); return false;
+}
+
+int rows = Print("Введите число строк: ");
+int columns = Print("Введите число столбцов: ");
+int[,] array = FillArray(rows, columns, 1, 9);
+PrintArray(array);
+Console.WriteLine("-------------");
+if (CheckLenghtArray(array, SwapRowsColumns(array))) PrintArray(SwapRowsColumns(array));
+*/
+
+// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+// 1, 2, 3 
+// 4, 6, 1 
+// 2, 1, 6
+
+// 1 встречается 3 раза 
+// 2 встречается 2 раз 
+// 3 встречается 1 раз 
+// 4 встречается 1 раз 
+// 6 встречается 2 раза
+/*
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = FillArray(rows, columns, min, max);
+PrintArray(array);
+int[] aftersortarray = DoSort(array);
+Console.WriteLine($"Вы ввели следующие числа: {string.Join(",", (aftersortarray))}");
+PrintData(aftersortarray);
+
+void PrintData(int[] inArray)
+{
+    int el = inArray[0];
+    int count = 1;
+    for (int i = 1; i < inArray.Length; i++)
+    {
+        if (inArray[i] != el)
+        {
+            Console.WriteLine($"{el} встречается {count}");
+            el = inArray[i];
+            count = 1;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"{el} встречается {count}");
+}
+
+int[] DoSort(int[,] inputArray)
+{
+    int index = 0;
+    int[] newarray = new int[inputArray.GetLength(0) * inputArray.GetLength(1)];
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            newarray[index] = inputArray[i, j];
+            index++;
+        }
+    }
+    Array.Sort(newarray);
+    return newarray;
+}
+
+Console.WriteLine();
+*/
+
+
+//Задача 59: Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец,
+//на пересечении которых расположен наименьший элемент массива.
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Минимальное возможное значение в двумерном массиве");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Максимальное возможное значение в двумерном массиве");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, columns, min, max);
+
+PrintArray(array);
+
+int [] indexes = GetIndexMinEl(array);
+
+PrintArray(GetResultArray(array,indexes));
+
+int[,] FillArray(int arrayRows, int arrayColumns, int minValue, int maxValue)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(minValue, maxValue + 1);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] GetIndexMinEl(int[,] inArray)
+{
+    int[] result = new int[]{0,0};
+    int min=inArray[0,0];
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if(min>inArray[i,j])
+            {
+                min=inArray[i,j];
+                result[0]=i;
+                result[1]=j;
+            }
+        }
+    }
+    return result;
+}
+
+int[,] GetResultArray(int[,] inArray, int[] indexes)
+{
+    int[,] result = new int[inArray.GetLength(0) - 1, inArray.GetLength(1) - 1];
+    int row = 0;
+    int column = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        if (i == indexes[0]) continue;
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (j == indexes[1]) continue;
+            result[row, column] = inArray[i, j];
+            column++;
+        }
+        column = 0;
+        row++;
+    }
+    return result;
+}
